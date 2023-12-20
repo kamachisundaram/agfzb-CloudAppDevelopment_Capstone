223c7a65bd7769ee870b828b678ba918d057a24b
kamachisundaram <m.kamachisundaram@gmail.com>
2023-12-20 21:31:14 +0000

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,9 +18,11 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):    
+    return render(request,'djangoapp/about.html')
 
+def contact(request):
+    return render(request,'djangoapp/contact.html')
 
 # Create a `contact` view to return a static contact page
 #def contact(request):
